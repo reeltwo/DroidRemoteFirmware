43d352d1e3c603fced47836882058a5fdeacff1e
rimim <nomail@thanks.com>
2022-09-04 07:00:28 -0400

    Added serial configuration commands #DRRESTART, #DRNAME, #DRSECRET, and #DRCONFIG

--- DroidRemoteFirmware.ino
+++ DroidRemoteFirmware.ino
@@ -29,10 +29,7 @@
 #include "ReelTwoSMQ32.h"
 #include "core/AnimatedEvent.h"
 #include "core/PinManager.h"
-#ifdef USE_SERVOS
-#include "ServoDispatchDirect.h"
-#include "ServoEasing.h"
-#endif
+#include "core/StringUtils.h"
 #include <Wire.h>
 
 ///////////////////////////////////
@@ -68,31 +65,47 @@ CommandScreenHandlerSSD1306 sDisplay(sPinManager);
 
 ////////////////////////////////
 
+#include <Preferences.h>
+Preferences preferences;
+#define PREFERENCE_REMOTE_HOSTNAME          "rhost"
+#define PREFERENCE_REMOTE_SECRET            "rsecret"
+
+////////////////////////////////
+
 void setup()
 {
     REELTWO_READY();
 
     PrintReelTwoInfo(Serial, "Droid Remote");
 
+    if (!preferences.begin("roamadome", false))
+    {
+        DEBUG_PRINTLN("Failed to init prefs");
+    }
+
     SetupEvent::ready();
 
     //Puts ESP in STATION MODE
     WiFi.mode(WIFI_STA);
-    SMQ::init(SMQ_HOSTNAME, SMQ_SECRET);
+    if (SMQ::init(preferences.getString(PREFERENCE_REMOTE_HOSTNAME, SMQ_HOSTNAME),
+                    preferences.getString(PREFERENCE_REMOTE_SECRET, SMQ_SECRET)))
+    {
+        printf("Droid Remote %s:%s\n",
+            preferences.getString(PREFERENCE_REMOTE_HOSTNAME, SMQ_HOSTNAME).c_str(),
+                preferences.getString(PREFERENCE_REMOTE_SECRET, SMQ_SECRET).c_str());
+        SMQ::setHostDiscoveryCallback([](SMQHost* host) {
+            if (host->hasTopic("BUTTON"))
+            {
+                printf("Discovered: %s\n", host->getHostName().c_str());
+                sMainScreen.addHost(host->getHostName(), host->getHostAddress());
+            }
+        });
 
-    SMQ::setHostDiscoveryCallback([](SMQHost* host) {
-        if (host->hasTopic("BUTTON"))
-        {
-            printf("Discovered: %s\n", host->getHostName().c_str());
-            sMainScreen.addHost(host->getHostName(), host->getHostAddress());
-        }
-    });
-
-    SMQ::setHostLostCallback([](SMQHost* host) {
-        printf("Lost: %s\n", host->getHostName().c_str());
-        sMainScreen.removeHost(host->getHostAddress());
-    });
-
+        SMQ::setHostLostCallback([](SMQHost* host) {
+            printf("Lost: %s\n", host->getHostName().c_str());
+            sMainScreen.removeHost(host->getHostAddress());
+        });
+    }
     Wire.begin();
 
     Wire.beginTransmission(SCREEN_ADDRESS);
@@ -135,10 +148,204 @@ SMQMESSAGE(EXIT, {
 
 ///////////////////////////////////////////////////////////////////////////////
 
+#define CONSOLE_BUFFER_SIZE                 300
+#define COMMAND_BUFFER_SIZE                 256
+
+static bool sNextCommand;
+static bool sProcessing;
+static unsigned sPos;
+static uint32_t sWaitNextSerialCommand;
+static char sBuffer[CONSOLE_BUFFER_SIZE];
+static bool sCmdNextCommand;
+static char sCmdBuffer[COMMAND_BUFFER_SIZE];
+
+static void runSerialCommand()
+{
+    sWaitNextSerialCommand = 0;
+    sProcessing = true;
+}
+
+static void resetSerialCommand()
+{
+    sWaitNextSerialCommand = 0;
+    sNextCommand = false;
+    sProcessing = (sCmdBuffer[0] == ':');
+    sPos = 0;
+}
+
+static void abortSerialCommand()
+{
+    sBuffer[0] = '\0';
+    sCmdBuffer[0] = '\0';
+    sCmdNextCommand = false;
+    resetSerialCommand();
+}
+
+void reboot()
+{
+    Serial.println(F("Restarting..."));
+    preferences.end();
+    ESP.restart();
+}
+
+void processConfigureCommand(const char* cmd)
+{
+    if (startswith_P(cmd, F("#DRRESTART")))
+    {
+        reboot();
+    }
+    else if (startswith_P(cmd, F("#DRNAME")))
+    {
+        String newName = String(cmd);
+        if (preferences.getString(PREFERENCE_REMOTE_HOSTNAME, SMQ_HOSTNAME) != newName)
+        {
+            preferences.putString(PREFERENCE_REMOTE_HOSTNAME, cmd);
+            printf("Changed.\n");
+            reboot();
+        }
+    }
+    else if (startswith_P(cmd, F("#DRSECRET")))
+    {
+        String newSecret = String(cmd);
+        if (preferences.getString(PREFERENCE_REMOTE_SECRET, SMQ_HOSTNAME) != newSecret)
+        {
+            preferences.putString(PREFERENCE_REMOTE_SECRET, newSecret);
+            printf("Changed.\n");
+            reboot();
+        }
+    }
+    else if (startswith_P(cmd, F("#DRCONFIG")))
+    {
+        printf("Name=%s\n", preferences.getString(PREFERENCE_REMOTE_HOSTNAME, SMQ_HOSTNAME).c_str());
+        printf("Secret=%s\n", preferences.getString(PREFERENCE_REMOTE_HOSTNAME, SMQ_SECRET).c_str());
+    }
+    else
+    {
+        Serial.println(F("Invalid"));
+    }
+}
+
+bool processDroidRemoteCommand(const char* cmd)
+{
+    switch (*cmd++)
+    {
+        default:
+            // INVALID
+            return false;
+    }
+    return true;
+}
+
+bool processCommand(const char* cmd, bool firstCommand)
+{
+    sWaitNextSerialCommand = 0;
+    if (*cmd == '\0')
+        return true;
+    if (!firstCommand)
+    {
+        if (cmd[0] != ':')
+        {
+            Serial.println(F("Invalid"));
+            return false;
+        }
+        return processDroidRemoteCommand(cmd+1);
+    }
+    switch (cmd[0])
+    {
+        case ':':
+            if (cmd[1] == 'D' && cmd[2] == 'R')
+                return processDroidRemoteCommand(cmd+3);
+            break;
+        case '#':
+            processConfigureCommand(cmd);
+            return true;
+        default:
+            Serial.println(F("Invalid"));
+            break;
+    }
+    return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 void loop()
 {
     AnimatedEvent::process();
     sDisplay.process();
     SMQ::process();
-}
+
+    if (Serial.available())
+    {
+        int ch = Serial.read();
+        if (ch == 0x0A || ch == 0x0D)
+        {
+            runSerialCommand();
+        }
+        else if (sPos < SizeOfArray(sBuffer)-1)
+        {
+            sBuffer[sPos++] = ch;
+            sBuffer[sPos] = '\0';
+        }
+    }
+    if (sProcessing && millis() > sWaitNextSerialCommand)
+    {
+        if (sCmdBuffer[0] == ':')
+        {
+            char* end = strchr(sCmdBuffer+1, ':');
+            if (end != nullptr)
+                *end = '\0';
+            if (!processCommand(sCmdBuffer, !sCmdNextCommand))
+            {
+                // command invalid abort buffer
+                DEBUG_PRINT(F("Unrecognized: ")); DEBUG_PRINTLN(sCmdBuffer);
+                abortSerialCommand();
+                end = nullptr;
+            }
+            if (end != nullptr)
+            {
+                *end = ':';
+                strcpy(sCmdBuffer, end);
+                DEBUG_PRINT(F("REMAINS: "));
+                DEBUG_PRINTLN(sCmdBuffer);
+                sCmdNextCommand = true;
+            }
+            else
+            {
+                sCmdBuffer[0] = '\0';
+                sCmdNextCommand = false;
+            }
+        }
+        else if (sBuffer[0] == ':')
+        {
+            char* end = strchr(sBuffer+1, ':');
+            if (end != nullptr)
+                *end = '\0';
+            if (!processCommand(sBuffer, !sNextCommand))
+            {
+                // command invalid abort buffer
+                DEBUG_PRINT(F("Unrecognized: ")); DEBUG_PRINTLN(sBuffer);
+                abortSerialCommand();
+                end = nullptr;
+            }
+            if (end != nullptr)
+            {
+                *end = ':';
+                strcpy(sBuffer, end);
+                sPos = strlen(sBuffer);
+                DEBUG_PRINT(F("REMAINS: "));
+                DEBUG_PRINTLN(sBuffer);
+                sNextCommand = true;
+            }
+            else
+            {
+                resetSerialCommand();
+                sBuffer[0] = '\0';
+            }
+        }
+        else
+        {
+            processCommand(sBuffer, true);
+            resetSerialCommand();
+        }
+    }}
 #pragma GCC diagnostic pop
